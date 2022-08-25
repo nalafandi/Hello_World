a190df0960775299b6433e1b032089ed17278d2a
niafa <niafa@DESKTOP-F1ARCRT>
2022-08-25 18:49:49 -0400

    Add project files.

---
+++ Hello_World/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Hello_World
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string stg = "Hello world (using string)";
+            Console.WriteLine(stg);
+            Console.WriteLine("Enter UserName: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Enter User Age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hello " + userName + " Your age is " + age);
+        }
+    }
+}
